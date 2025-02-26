3a7611690f76a5f680e490b29e1483555b4957a0
arthur <artur.gotovchits@mail.ru>
2025-02-26 17:11:07 +0300

    Init project.

--- src/main/java/com/company/store/model/BaseEntity.java
+++ src/main/java/com/company/store/model/BaseEntity.java
@@ -22,6 +22,6 @@ public abstract class BaseEntity {
     private LocalDateTime createdAt;
 
     @LastModifiedDate
-    @Column(name = "updated_at", nullable = false, updatable = false)
+    @Column(name = "updated_at", nullable = false)
     private LocalDateTime updatedAt;
 }
